b0f20c7a53b32288b9ca1a84e6cb774668fa29dd
jhaybie <jhay.basco@icloud.com>
2013-10-24 09:37:00 -0500

    cleaned up more code

--- Gesundheit/TALocation.m
+++ Gesundheit/TALocation.m
@@ -11,31 +11,31 @@
 @implementation TALocation
 
 
-- (NSString *) getCurrentLocationZip {
-    NSString *zip = @"60076";
-    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
-    [locationManager startUpdatingLocation];
-    CLLocation *location = [locationManager location];
-    CLLocationCoordinate2D coordinate = [location coordinate];
-    NSDictionary *address;
-    
-    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
-    
-    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
-        if (error){
-            NSLog(@"Geocode failed with error: %@", error);
-            NSLog(@"Error acquiring location.");
-        return;
-        }
-        NSLog(@"Received placemarks: %@", placemarks);
-        
-    }];
-    return zip;
-}
+//- (NSString *) getCurrentLocationZip {
+//    NSString *zip = @"60076";
+//    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+//    [locationManager startUpdatingLocation];
+//    CLLocation *location = [locationManager location];
+//    CLLocationCoordinate2D coordinate = [location coordinate];
+//    NSDictionary *address;
+//    
+//    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+//    
+//    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+//        if (error){
+//            NSLog(@"Geocode failed with error: %@", error);
+//            NSLog(@"Error acquiring location.");
+//        return;
+//        }
+//        NSLog(@"Received placemarks: %@", placemarks);
+//        
+//    }];
+//    return zip;
+//}
 
 -(NSArray *) fetchPollenData {
-    NSArray *forecastArray;
-    NSString *zip = [self getCurrentLocationZip];
+    NSMutableArray *weeklyForecast;
+    NSString *zip = @"60654";// [self getCurrentLocationZip];
     NSString *address = [NSString stringWithFormat:@"http://direct.weatherbug.com/DataService/GetPollen.ashx?zip=%@", zip];
     NSURL *url = [NSURL URLWithString:address];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
@@ -43,9 +43,15 @@
     [NSURLConnection sendAsynchronousRequest:request
                                        queue:[NSOperationQueue mainQueue]
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-     }];
-    
-    return forecastArray;
+        NSDictionary *initialDump = [NSJSONSerialization JSONObjectWithData:data
+                                                                    options:0
+                                                                      error:&connectionError];
+        NSArray *forecastArray = [initialDump objectForKey:@"dayList"];
+        NSLog(@"%@, %@", [initialDump objectForKey:@"city"], [initialDump objectForKey:@"state"]);
+        NSLog(@"%@", [forecastArray[0] desc]);
+        //return forecastArray;
+    }];
+    return weeklyForecast;
 }
 
 @end
