0fb0e5642a593973b144e611f861d50a77ec211d
jhaybie <jhay.basco@icloud.com>
2013-10-25 12:54:07 -0500

    additions/revisions to storyboard

---
+++ Gesundheit/TAWeeklyForecastViewController.h
@@ -0,0 +1,13 @@
+//
+//  TAWeeklyForecastViewController.h
+//  Gesundheit
+//
+//  Created by Jhaybie on 10/25/13.
+//  Copyright (c) 2013 Jhaybie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAWeeklyForecastViewController : UIViewController
+
+@end
---
+++ Gesundheit/TAWeeklyForecastViewController.m
@@ -0,0 +1,25 @@
+//
+//  TAWeeklyForecastViewController.m
+//  Gesundheit
+//
+//  Created by Jhaybie on 10/25/13.
+//  Copyright (c) 2013 Jhaybie. All rights reserved.
+//
+
+#import "TAWeeklyForecastViewController.h"
+
+@interface TAWeeklyForecastViewController ()
+
+@end
+
+@implementation TAWeeklyForecastViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)onViewTap:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
