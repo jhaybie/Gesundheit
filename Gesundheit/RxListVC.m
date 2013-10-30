819bf8ebb24c16f8f1e054501a1efa1936a31cf1
deebost <floggingdan721@yahoo.com>
2013-10-30 14:09:57 -0500

    commit

--- Gesundheit/RxListVC.m
+++ Gesundheit/RxListVC.m
@@ -26,7 +26,7 @@
 - (IBAction)onBackButtonTap:(id)sender;
 @end
 
-
+@implementation RxListVC
 @synthesize  backroundImage,
              city,
              state,
@@ -113,43 +113,6 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *tempState = [[tempDrugstore.address componentsSeparatedByString:@", "] objectAtIndex:2];
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%@\n%@, %@", tempAddress1, tempCity, tempState];
     return cell;
-}
-
-- (void)fetchSearchResults {
-    drugstores = [[NSMutableArray alloc] init];
-    city = [city stringByReplacingOccurrencesOfString:@" "
-                                           withString:@"+"];
-    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/textsearch/json?query=pharmacies+in+%@+%@&sensor=true&key=AIzaSyChk-7Q-sBiibQi8sUHWb7g3bHc2U1WdPQ", city, state]]]
-                                       queue:[NSOperationQueue mainQueue]
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-                               NSDictionary *initialDump = [NSJSONSerialization JSONObjectWithData:data
-                                                                                           options:0
-                                                                                             error:&connectionError];
-                               searchResults = [initialDump objectForKey:@"results"];
-                               for (int i = 0; i < searchResults.count; i++) {
-                                   NSDictionary *tempDict = [searchResults[i] objectForKey:@"geometry"];
-                                   Drugstore *tempRx = [[Drugstore alloc] init];
-                                   tempRx.name = [searchResults[i] objectForKey:@"name"];
-                                   tempRx.address = [searchResults[i] objectForKey:@"formatted_address"];
-                                   tempRx.coord = CLLocationCoordinate2DMake([[[tempDict objectForKey:@"location"] objectForKey:@"lat"] floatValue], [[[tempDict objectForKey:@"location"] objectForKey:@"lng"] floatValue]);
-                                   tempRx.openNow = (BOOL)[[searchResults[i] objectForKey:@"opening_hours"] objectForKey:@"open_now"];
-                                   [drugstores addObject:tempRx];
-                               }
-                               [drugstoresTableView reloadData];
-                           }];
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    backroundImage.image = [UIImage imageNamed:[NSString stringWithFormat:@"skyBackRoundwithClouds.png"]];
-    drugstoresTableView.alpha = .85;
-
-    [self fetchSearchResults];
-}
-
-- (IBAction)onBackButtonTap:(id)sender {
-    [self dismissViewControllerAnimated:YES
-                             completion:nil];
 }
 
 @end
