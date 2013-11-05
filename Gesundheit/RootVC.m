34488070c1198b04adaa4e3facedfebc721ec683
jhaybie <jhay.basco@icloud.com>
2013-11-04 20:37:50 -0600

    added sample technical interview code (found at very bottom of RootVC)
    You’re welcome!

--- Gesundheit/RootVC.m
+++ Gesundheit/RootVC.m
@@ -421,4 +421,16 @@ NSString          *city,
     [[NSNotificationCenter defaultCenter] removeObserver:observer];
 }
 
+/*   --------------------  DELETE ME  --------------------------
+ for (int i = 0; i < 100; i++) {
+     if (i % 3 == 0 && i % 5 == 0)
+         NSLog(@"Multiple of 3 AND 5");
+     else if (i % 3 == 0)
+         NSLog(@"Multiple of 3");
+     else if (i % 5 == 0)
+         NSLog(@"Multiple of 5");
+     else NSLog(@"%i", i);
+ }
+     --------------------  DELETE ME  -------------------------- */
+
 @end
